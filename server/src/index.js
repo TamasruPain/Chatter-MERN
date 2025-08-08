4de706bc8bb1e48e0f21c33b35942051de656a9e
Tamasru Pain <tamasrupain02@gmail.com>
2025-08-08 17:24:56 +0530

    backend update: Fix database connection and server

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+
 const connectDB = require('./lib/db');
+
 const authRoutes = require('./routes/auth.route');
 const messageRoutes = require('./routes/message.route');
+
 require('dotenv').config();
 
 const app = express();
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        
+        "https://chatter-bird-chatapp.vercel.app"
     ],
     credentials: true
 }));
@@ -25,10 +29,19 @@ const initSocket = require('./lib/socket');
 const { io, server } = initSocket(app);
 
 const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-    connectDB();
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+        server.listen(port, () => {
+            console.log(`Server running on port ${port}`);
+        });
+    } catch (error) {
+        console.error("Failed to connect to DB:", error);
+        process.exit(1);  // optional: stop app if DB connection fails
+    }
+};
+
+startServer();
 
 //heath check endpoint
 app.get('/health', (req, res) => {
--- server/src/lib/db.js
+++ server/src/lib/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try{
-        const connect = await mongoose.connect(process.env.MONGO_URI);
-       console.log('MongoDB connected');
-    }catch(error){
-       console.log("MongoDB connection failed", error);
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.log("MongoDB connection failed", error);
+        throw error; 
     }
 }
 
